f632fd60a3b67d8b3efcb5ce63f70e40cb4fab23
Emiliano Caballero <emiliano.caballero98@gmail.com>
2023-12-28 18:26:43 -0300

    Se creo la posibilidad de crear platos nuevos y poder guardarlos

--- Controllers/DishController.cs
+++ Controllers/DishController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Restaurant.Models;
+using Restaurant.Models.ViewModels;
 
 namespace Restaurant.Controllers
 {
@@ -15,6 +17,41 @@ namespace Restaurant.Controllers
             var dishes = _context.Dishes.Include(x => x.DishCategoryNavigation );
 
             return View( await dishes.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["PlatoNuevo"] = new SelectList(_context.DishCategories, "DishCategoryId", "DishCategoryName");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(DishViewModel model)
+        {
+
+            if(ModelState.IsValid)
+            {
+                var dish = new Dish()
+                {
+                    DishName = model.DishName,
+                    DishCategory = model.DishCategoryId,
+                    DishFinishTime = model.DishFinishTime,
+                    DishPrice = model.DishPrice,
+                    DishTac = model.DishTac
+                };
+
+                _context.Add(dish);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(GetDishes));
+            }
+
+            ViewData["PlatoNuevo"] = new SelectList(_context.DishCategories, "DishCategoryId", "DishCategoryName", model.DishCategoryId);
+
+            return View();
         }
     }
 }
